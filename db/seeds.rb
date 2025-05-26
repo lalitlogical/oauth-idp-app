e934715e4d10c35991bb123cbf4bf07590904d46
Lalit Kumar Maurya <lalit.logical@gmail.com>
2025-05-26 23:47:48 +0530

    TLS fake certificate support

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ Rails.logger.info "User created with ID: #{user.id}"
 
 # ✅ OIDC Interactive App (User Login)
 application = Doorkeeper::Application.find_or_create_by!(name: "Web Frontend App") do |application|
-  application.redirect_uri = "http://localhost:3001/oauth/callback http://client.myapp.local/oauth/callback http://client.lalit.local/oauth/callback"
+  application.redirect_uri = "http://localhost:3001/oauth/callback http://client.myapp.local/oauth/callback http://client.lalit.local/oauth/callback https://client.lalit.local/oauth/callback"
   application.scopes = "openid profile email records"
   application.confidential = false
   application.uid = 'web-frontend-app-client-id'
